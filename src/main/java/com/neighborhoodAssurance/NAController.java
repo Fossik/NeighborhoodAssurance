b0806523a7c518b83f3377b6ec6fdbb23ab53885
Administrator <Administrator@DESKTOP-099UQRG>
2020-02-09 22:12:27 -0500

    added controller

--- src/main/java/com/neighborhoodAssurance/NAController.java
+++ src/main/java/com/neighborhoodAssurance/NAController.java
@@ -2,12 +2,22 @@ package com.neighborhoodAssurance;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class NAController {
 
-	@RequestMapping("/index")
+	@RequestMapping(value="/index", method=RequestMethod.GET) //add params later ex: params= {"loyalty=blue"}
 	public String index() {
 		return "index";
+		
 	}
+	@RequestMapping(value="/index", method=RequestMethod.GET, headers= {"content-type=text/json"}) 
+	public String readJSON() {
+		return "index";
+	}
+	/*@RequestMapping(value="/index", method=RequestMethod.GET, params= {"loyalty=blue"}) 
+	public String readBlue() {
+		return "index";
+	} this is just an example */
 }
