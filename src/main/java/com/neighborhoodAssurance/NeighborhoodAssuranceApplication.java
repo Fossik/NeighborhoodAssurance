6d4ae7d98235962fad203b53e2aff35aa7ac105b
Administrator <Administrator@DESKTOP-099UQRG>
2020-02-09 20:43:33 -0500

    Initial commit of Spring Boot Project

---
+++ src/main/java/com/neighborhoodAssurance/NeighborhoodAssuranceApplication.java
@@ -0,0 +1,13 @@
+package com.neighborhoodAssurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeighborhoodAssuranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeighborhoodAssuranceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neighborhoodAssurance/neighborhoodAssurance/NeighborhoodAssuranceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neighborhoodAssurance.neighborhoodAssurance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeighborhoodAssuranceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
