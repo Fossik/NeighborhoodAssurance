a52dc9162f584e326043d015cd43cccc2f651a63
Administrator <Administrator@DESKTOP-099UQRG>
2020-02-24 22:08:23 -0500

    test branch

--- src/main/java/com/neighborhoodAssurance/NeighborhoodAssuranceApplication.java
+++ src/main/java/com/neighborhoodAssurance/NeighborhoodAssuranceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NeighborhoodAssuranceApplication {
-
+//test branch
 	public static void main(String[] args) {
 		SpringApplication.run(NeighborhoodAssuranceApplication.class, args);
 	}
