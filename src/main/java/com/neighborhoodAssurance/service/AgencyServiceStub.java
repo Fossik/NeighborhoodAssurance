e5e1d76d92ace08b051e664c0a57442e4893ebe7
rosasmn <rosasmn@mail.uc.edu>
2020-03-22 13:25:03 -0400

    Creating AgencyDTO method in AgencyServiceStub

--- src/main/java/com/neighborhoodAssurance/service/AgencyServiceStub.java
+++ src/main/java/com/neighborhoodAssurance/service/AgencyServiceStub.java
@@ -1,5 +1,17 @@
 package com.neighborhoodAssurance.service;
 
+import com.neighborhoodAssurance.dto.AgencyDTO;
+
 public class AgencyServiceStub {
 
+	public AgencyDTO fetchByOri(String ori) {
+		AgencyDTO agencyDTO = new AgencyDTO();
+		agencyDTO.setOri(ori);
+		return agencyDTO;
+	}
+	
+	public void save(AgencyDTO agencyDTO) {
+		
+	}
+	
 }
