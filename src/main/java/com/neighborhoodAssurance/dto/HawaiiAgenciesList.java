596177b8721e7524c66e7002687557e763b25e07
rosasmn <rosasmn@mail.uc.edu>
2020-04-22 16:47:46 -0400

    Creating a DTO for Hawaii Agencies and a list DTO for it

---
+++ src/main/java/com/neighborhoodAssurance/dto/HawaiiAgenciesList.java
@@ -0,0 +1,17 @@
+package com.neighborhoodAssurance.dto;
+
+import java.util.List;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class HawaiiAgenciesList {
+	
+	@SerializedName("HI")
+	@Expose
+	private List<HawaiiAgencies> hiAgencies = null;
+	
+	public List<HawaiiAgencies> getHIAgencies() {
+		return hiAgencies;
+	}
+}
