3c41962c6087aed2778585f4c6f500e2e0e857b4
Michael Lawson <lawsonm6@mail.uc.edu>
2020-04-24 20:28:36 -0400

    Fixed merge errors

--- src/main/java/com/neighborhoodAssurance/NAController.java
+++ src/main/java/com/neighborhoodAssurance/NAController.java
@@ -41,7 +41,6 @@ public class NAController {
 		logger.info("Request successful we got: " + searchState + ", " + searchYearFrom + ", " + searchYearTo);
 		return "searchresults";
 	}
-	
 
 	@RequestMapping(path = "/searchResults/{state}/{startingYear}/{endingYear}", method = RequestMethod.GET)
 	public String searchResults(@PathVariable String state, @PathVariable int startingYear, @PathVariable int endingYear) {
@@ -56,44 +55,10 @@ public class NAController {
 		}
 		return "searchResults";
 	}
-	@RequestMapping(value="/about", method=RequestMethod.GET)
-	public String goToAbout(@RequestParam int goToAbout) {
-		logger.info("Successful navigation to the About page.");
-		return "about";
-
-	}
-	
-
-package com.neighborhoodAssurance;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RequestParam;
-
-@Controller
-public class NAController {
-
-	Logger logger = LoggerFactory.getLogger(NAController.class);
-	
-	@RequestMapping(value="/home", method=RequestMethod.GET) 
-	public String readJSON() {
-		return "home";
-	}
-
-	@RequestMapping(value="/searchresults", method=RequestMethod.GET)
-	public String doSearch(@RequestParam String searchState, @RequestParam String searchYearFrom, @RequestParam String searchYearTo, @RequestParam int searchGo) {
-		logger.info("Request successful! We got: " + searchState + ", " + searchYearFrom + ", " + searchYearTo);
-		return "searchresults";
-	}
 	
 	@RequestMapping(value="/about", method=RequestMethod.GET)
 	public String goToAbout(@RequestParam int goToAbout) {
 		logger.info("Successful navigation to the About page.");
 		return "about";
 	}
-
 }
